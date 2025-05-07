a8bc96a79a7a7fcc342085bb7fe4ae915e7833c5
rafaeltaciel <62748942+rafaeltaciel@users.noreply.github.com>
2025-05-07 02:51:48 -0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,40 +1,15 @@
-name: Python application
+from setuptools import setup
 
-on:
-  push:
-    branches:
-      - "main"
-  pull_request:
-    branches:
-      - "main"
+APP = ['main.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': [],
+}
 
-permissions:
-  contents: read
-
-jobs:
-  build:
-    runs-on: macos-latest  # Importante! Mude para macOS!
-
-    steps:
-    - uses: actions/checkout@v4
-
-    - name: Set up Python 3.10
-      uses: actions/setup-python@v3
-      with:
-        python-version: "3.10"
-
-    - name: Install dependencies
-      run: |
-        python -m pip install --upgrade pip
-        pip install -r requirements.txt || true  # Ignora se não tiver
-        pip install py2app
-
-    - name: Build macOS app
-      run: |
-        python setup.py py2app
-
-    - name: Upload build artifact
-      uses: actions/upload-artifact@v3
-      with:
-        name: MeuAppMac
-        path: dist/*.app
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
